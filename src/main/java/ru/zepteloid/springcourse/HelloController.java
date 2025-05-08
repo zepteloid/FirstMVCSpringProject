51cea806710a2c99742a4efacad02cddeee48603
eJwtzMtuwyAQQNF/mTULWzHG4R/6UNIoVTYIw+BSEw8a7Dwa5d8rRd5e6dwHWOewlC8acQINrVXB11XTyX4bVONVwM4G3Lpu42TnpVJt39SVBwGZ6RI98kof68nM62r4IZPb3eV02+HveDi+bU6f1fee9/JDvdOQFln/4RxbEPAiZr5nBA09WkYGAcXRK1h/jpPOS5+iMyPeBaP1mngQjJnEUpA1nm1M4ko8hkRXEIC3HBmLsTPoaUnp+fwHz2FQ0g== <vadimbakulin2006@gmail.com>
2025-05-08 17:09:07 +0300

    First commit with solution for version Tomcat and Java

---
+++ src/main/java/ru/zepteloid/springcourse/HelloController.java
@@ -0,0 +1,15 @@
+package ru.zepteloid.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
+
